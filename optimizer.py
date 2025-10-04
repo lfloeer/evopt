6800f480a1e4e27fb7efac3e987b96f130cd324a
ekke <165339945+ekkea@users.noreply.github.com>
2025-10-04 09:35:41 +0200

    Improve target function factor for early charging (#30)

--- optimizer.py
+++ optimizer.py
@@ -196,7 +196,7 @@ class Optimizer:
         if self.strategy.charging_strategy == 'charge_before_export':
             for i, bat in enumerate(self.batteries):
                 for t in self.time_steps:
-                    objective += self.variables['c'][i][t] * self.min_import_price * 1e-6 * (self.T - t)
+                    objective += self.variables['c'][i][t] * self.min_import_price * 1.5e-5 * (self.T - t)
 
         # prefer charging at high solar production times to unload public grid from peaks
         if self.strategy.charging_strategy == 'attenuate_grid_peaks':
