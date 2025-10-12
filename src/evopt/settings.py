d16b8cd9d58e5748e9d6068813950152ec561810
Lars <github@lfloeer.de>
2025-10-12 11:51:42 +0000

    settings

---
+++ src/evopt/settings.py
@@ -0,0 +1,9 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class OptimizerSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="OPTIMIZER_")
+
+    num_threads: int | None = Field(default=None, description="Number of threads to use for optimization")
+    time_limit: int | None = Field(default=None, description="Time limit for the optimization process in seconds")
