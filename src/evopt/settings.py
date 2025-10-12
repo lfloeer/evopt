f24f6e7feeaca5b0e1b442d3947ca04f79c0eb44
Lars <github@lfloeer.de>
2025-10-12 12:18:54 +0000

    correct type

--- src/evopt/settings.py
+++ src/evopt/settings.py
@@ -6,4 +6,4 @@ class OptimizerSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="OPTIMIZER_")
 
     num_threads: int | None = Field(default=None, description="Number of threads to use for optimization")
-    time_limit: int | None = Field(default=None, description="Time limit for the optimization process in seconds")
+    time_limit: float | None = Field(default=None, description="Time limit for the optimization process in seconds")
