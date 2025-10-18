33cb0839c1b06b6fd840945ccb2f7a4d0c1910f8
Lars <github@lfloeer.de>
2025-10-18 16:56:09 +0000

    make expected response optional

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,13 +14,15 @@ def test_optimizer(test_case: pathlib.Path):
 
     test_data = json.loads(test_case.read_text())
 
-    request = test_data.get("request", {})
-    expected_response = test_data.get("expected_response", {})
+    request = test_data["request"]
+    expected_response = test_data.get("expected_response")
 
     response = client.post("/optimize/charge-schedule", json=request)
 
     assert response.status_code == 200
-    assert response.json["status"] == expected_response.get("status", {})
-    assert numpy.isclose(response.json["objective_value"],
-                         expected_response.get("objective_value", {}),
-                         rtol=1e-05, atol=1e-08, equal_nan=False)
+
+    if expected_response is not None:
+        assert response.json["status"] == expected_response.get("status", {})
+        assert numpy.isclose(response.json["objective_value"],
+                             expected_response.get("objective_value", {}),
+                             rtol=1e-05, atol=1e-08, equal_nan=False)
