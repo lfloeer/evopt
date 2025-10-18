1dfa962978a25aedfa5a5b038e3c0084044b854f
Lars <github@lfloeer.de>
2025-10-18 16:54:27 +0000

    simplify test cases

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
-import glob
+
 import json
+import pathlib
 
 import numpy
 import pytest
@@ -7,15 +8,11 @@ import pytest
 from evopt.app import app
 
 
-@pytest.mark.parametrize('test_case', glob.glob('test_cases/*.json'))
-def test_optimizer(test_case: str):
+@pytest.mark.parametrize('test_case', pathlib.Path('test_cases').glob('*.json'))
+def test_optimizer(test_case: pathlib.Path):
     client = app.test_client()
 
-    response = None
-    print(test_case)
-    with open(f'{test_case}') as f:
-        test_data = json.load(f)
-        f.close()
+    test_data = json.loads(test_case.read_text())
 
     request = test_data.get("request", {})
     expected_response = test_data.get("expected_response", {})
